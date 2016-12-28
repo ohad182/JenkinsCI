932bfe9c7c46556182838b53a2c92d0ba4258da0
ohad182 <ohad182@gmail.com>
2016-12-28 18:18:05 +0200

    Update helloWorld.groovy

--- src/com/marvell/CIUtils/helloWorld.groovy
+++ src/com/marvell/CIUtils/helloWorld.groovy
@@ -1,5 +1,5 @@
 // src/com/marvell/CIUtils/helloWorld.groovy
 def call(name) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
-    echo "Hello world, ${name}"
+    echo "Hello world 111, ${name}"
 }
