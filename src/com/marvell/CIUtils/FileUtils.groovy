0af9022f35268e6ff11e5d8b265d98029b08b2f5
ohad182 <ohad182@gmail.com>
2017-01-01 15:28:55 +0200

    Update FileUtils.groovy

--- src/com/marvell/CIUtils/FileUtils.groovy
+++ src/com/marvell/CIUtils/FileUtils.groovy
@@ -1,12 +1,41 @@
 package com.marvell.utils
 
-import  java.nio.file.*
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
 
 class FileUtils{
 
     def deleteIfExists(filePath){
         File file = new File(filePath)
         return Files.deleteIfExists(file.toPath())
+    }
+
+   def printInfo(){
+        Path dir = Paths.get('resources') 
+        BasicFileAttributes attrs = Files.readAttributes(dir, BasicFileAttributes) 
+        
+        println """
+        Directory name: ${dir.fileName}
+        Absolute path: ${dir.toAbsolutePath()}
+        
+        The file exists: ${Files.exists(dir)}
+        The file is readable: ${Files.isReadable(dir)}
+        The file is writable: ${Files.isWritable(dir)}
+        The file is executable: ${Files.isExecutable(dir)}
+        The file is a directory: ${Files.isDirectory(dir)}
+        
+        Created: ${attrs.creationTime()}
+        Last modified: ${attrs.lastModifiedTime()}
+        Last accessed: ${attrs.lastAccessTime()}
+        
+        Children:"""
+        
+        
+        dir.eachFile {
+            println "  - ${it.fileName}"
+        }
     }
 
     def createOrUpdate(directory, fileName, extension, infoList) {
