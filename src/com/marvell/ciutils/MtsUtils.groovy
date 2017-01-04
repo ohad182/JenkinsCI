5c0fb6b6dc7c25eecfbdfd08cbf74ff6faacc4c3
ohad182 <ohad182@gmail.com>
2017-01-04 17:55:59 +0200

    Update MtsUtils.groovy

--- src/com/marvell/ciutils/MtsUtils.groovy
+++ src/com/marvell/ciutils/MtsUtils.groovy
@@ -14,6 +14,6 @@ class MtsUtils{
              build_type is ${env.BUILD_TYPE}
       """
     //send to the .pl these three vars
-      echo new File('//fileril103/dev/TOOLS/cc1tools/utils/auto_compile_git/Start_Build_Bx.pl').text
+      print new File('//fileril103/dev/TOOLS/cc1tools/utils/auto_compile_git/Start_Build_Bx.pl').text
   }
 }
