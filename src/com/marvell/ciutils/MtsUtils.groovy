8eb10b576b8ec7a87679d7421212a104cd2886d6
ohad182 <ohad182@gmail.com>
2017-01-04 17:33:20 +0200

    Update MtsUtils.groovy

--- src/com/marvell/ciutils/MtsUtils.groovy
+++ src/com/marvell/ciutils/MtsUtils.groovy
@@ -9,9 +9,9 @@ class MtsUtils{
   
   def startBuild(){
     steps.echo '''
-      box_name is $BOX_NAME
-      box branch is $BOX_BRANCH
-      build_type is $BUILD_TYPE
+      box_name is $steps.env.BOX_NAME
+      box branch is $steps.env.BOX_BRANCH
+      build_type is $steps.env.BUILD_TYPE
       '''
       
       steps.echo new File('//fileril103/dev/TOOLS/cc1tools/utils/auto_compile_git/Start_Build_Bx.pl').text
