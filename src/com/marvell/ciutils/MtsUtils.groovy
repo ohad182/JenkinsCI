ac6c7bf414cd04055cee963744765a9d30b51fed
ohad182 <ohad182@gmail.com>
2017-01-05 16:34:14 +0200

    Update MtsUtils.groovy

--- src/com/marvell/ciutils/MtsUtils.groovy
+++ src/com/marvell/ciutils/MtsUtils.groovy
@@ -8,12 +8,12 @@ class MtsUtils{
     }
   
   def startBuild(printer){
-    printer.print """
+    printer.addShortText """
              box_name is ${env.BOX_NAME}
              box branch is ${env.BOX_BRANCH}
              build_type is ${env.BUILD_TYPE}
       """
     //send to the .pl these three vars
-      printer.print new File('//fileril103/dev/TOOLS/cc1tools/utils/auto_compile_git/Start_Build_Bx.pl').text
+      printer.addShortText new File('//fileril103/dev/TOOLS/cc1tools/utils/auto_compile_git/Start_Build_Bx.pl').text
   }
 }
